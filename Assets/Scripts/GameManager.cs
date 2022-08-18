a1088a8a192ee5de99743aad93e83a538e5b31d1
Nuno "Omega Leo" Diogo <nunodiogo@omegaleo.pt>
2022-08-18 23:03:28 +0100

    Day 3 progress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Canvas _canvas;
+
+    [Header("UI")] 
+    [SerializeField] private GameObject _scanlines;
+
+    private void Start()
+    {
+        _canvas = GetComponent<Canvas>();
+
+        if (PlayerPrefs.HasKey("ScanLines"))
+        {
+            var scanLinesActive = PlayerPrefs.GetString("ScanLines");
+            if (bool.TryParse(scanLinesActive, out var active))
+            {
+                _scanlines.SetActive(active);
+            }
+        }
+    }
+
+    public void ToggleScanLines()
+    {
+        bool active = !_scanlines.activeSelf;
+        _scanlines.SetActive(active);
+        PlayerPrefs.SetString("ScanLines", active.ToString());
+    }
+}
